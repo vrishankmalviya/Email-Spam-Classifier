1b51a64646968e097da84b00ae53e5ca19f150b1
Vrishank Malviya <vrishankmalviya@gmail.com>
2025-12-11 21:55:09 +0530

    Update preprocess.py

--- app/preprocess.py
+++ app/preprocess.py
@@ -1,4 +1,4 @@
-import nltk
+'''import nltk
 from nltk.corpus import stopwords
 import string
 
@@ -30,4 +30,47 @@ def transform_text(text):
     for i in text:
         y.append(ps.stem(i))
         
-    return ( y)
+    return ( y)'''
+
+
+import nltk
+from nltk.corpus import stopwords
+import string
+from nltk.stem.porter import PorterStemmer
+
+
+try:
+    stopwords.words('english')
+except LookupError:
+    nltk.download('stopwords')
+
+try:
+    nltk.word_tokenize("test")
+except LookupError:
+    nltk.download('punkt')
+
+ps = PorterStemmer()
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return y
