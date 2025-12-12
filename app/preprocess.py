0265a5ef70179177c9d510f32471350b0d36cd1d
vrishankmalviya <vrishankmalviya@gmail.com>
2025-12-12 21:09:27 +0530

    Update preprocess.py

--- app/preprocess.py
+++ app/preprocess.py
@@ -1,34 +1,50 @@
 import nltk
 from nltk.corpus import stopwords
 import string
-
 from nltk.stem.porter import PorterStemmer
+import streamlit as st
+
 ps = PorterStemmer()
 
+@st.cache_resource
+def ensure_nltk_data():
+    
+    for res in ["punkt_tab", "punkt"]:
+        try:
+            nltk.data.find(f"tokenizers/{res}")
+        except LookupError:
+            nltk.download(res, quiet=True)
+
+    # Ensure stopwords corpus
+    try:
+        nltk.data.find("corpora/stopwords")
+    except LookupError:
+        nltk.download("stopwords", quiet=True)
+
+
+ensure_nltk_data()
+
 def transform_text(text):
-    text=text.lower()
-    
+    text = text.lower()
+
     text = nltk.word_tokenize(text)
-    
-    y=[]
+
+    y = []
     for i in text:
         if i.isalnum():
             y.append(i)
-    
-    
-    text =y[:]
+
+    text = y[:]
     y.clear()
-    
+
     for i in text:
-        if i not in stopwords.words('english') and i not in string.punctuation:
+        if i not in stopwords.words("english") and i not in string.punctuation:
             y.append(i)
-    
-    
-    text =y[:]
+
+    text = y[:]
     y.clear()
-    
+
     for i in text:
         y.append(ps.stem(i))
-        
-    return ( y)
 
+    return y
